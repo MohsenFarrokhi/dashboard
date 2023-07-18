926b1c75dfbd6aff56f7cba238ab8d381ed6aba0
Mohsen Farrokhi <mfarrokhi1996@gmail.com>
2023-07-18 12:42:12 +0330

    Fix: add font and styles to Contact.jsx

--- vite-project/src/components/Contact.jsx
+++ vite-project/src/components/Contact.jsx
@@ -3,15 +3,24 @@ import {Link} from "react-router-dom";
 
 const Contact = ({name, number, id, onDelete}) => {
     return (
-        <div className="card">
-            <Link to={`/contact/${id}`}>
+        <div className="ContactCard">
+            <Link to={`/contact/${id}`} className="ContactLink">
                 <p>Name : {name}</p>
                 <p>Phone Number : {number}</p>
             </Link>
-            <button onClick={onDelete}>Delete</button>
-            <Link to={`/edit/${id}`}>
-                <button>Edit</button>
-            </Link>
+            <div className="ContactBtn">
+                <button
+                    onClick={onDelete}
+                    className="DeleteBtn">
+                    Delete
+                </button>
+                <Link to={`/edit/${id}`}>
+                    <button
+                        className="EditBtn"
+                    >Edit
+                    </button>
+                </Link>
+            </div>
         </div>
     )
 }
